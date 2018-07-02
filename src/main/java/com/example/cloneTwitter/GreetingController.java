3e1334141850f5daa7c2fdca209215bbfb017486
Olimpik <olimpiewow@mail.ru>
2018-07-02 17:37:06 +0300

    version 3

--- src/main/java/com/example/cloneTwitter/GreetingController.java
+++ src/main/java/com/example/cloneTwitter/GreetingController.java
@@ -1,14 +1,23 @@
 package com.example.cloneTwitter;
 
+import com.example.cloneTwitter.domain.Message;
+import com.example.cloneTwitter.repos.MessageRepo;
+import com.sun.org.apache.xml.internal.serializer.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
+
+
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -16,6 +25,23 @@ public class GreetingController {
     ) {
         model.put("name", name);
         return "greeting";
+    }
+    @GetMapping
+    public String main(Map<String, Object> model){
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag);
+
+                messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", Utils.messages);
+        return "main";
     }
 
 }
