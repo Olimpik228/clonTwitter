748871fa3b44598110a70ec7e4cc2a98dcb6ca63
Olimpik <olimpiewow@mail.ru>
2018-07-08 18:53:48 +0300

    Spring Boot FlyWay: миграции БД, профиль пользователя

--- src/main/java/com/example/cloneTwitter/controller/UserController.java
+++ src/main/java/com/example/cloneTwitter/controller/UserController.java
@@ -3,9 +3,11 @@ package com.example.cloneTwitter.controller;
 import com.example.cloneTwitter.domain.Role;
 import com.example.cloneTwitter.domain.User;
 import com.example.cloneTwitter.repos.UserRepo;
+import com.example.cloneTwitter.service.UserSevice;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,20 +17,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserSevice userSevice;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userSevice.findAll());
 
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -36,40 +40,34 @@ public class UserController {
 
         return "userEdit";
     }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String,String> form,
             @RequestParam("userId") User user
     ) {
-        user.setUsername(username);           // устанавливаем пользователю имя, которое пришло с фронта
-        //берем все существующие в приложении роли,
-        // преобразуем массив этих ролей в стрим (java stream api),
-        // где получаем имена ролей и полученый список имён складываем в set
-Set<String> roles = Arrays.stream(Role.values())
-        .map(Role::name)
-        .collect(Collectors.toSet());
-
-user.getRoles().clear(); //очищаем персистентную коллекцию от установленных ролей
-//обходим список полей, пришедших от пользователя,
-// проверяем, если имя какого-то поля является именем роли,
-// то ищем такую роль в enum Role и устанавливаем эти роли пользователю.
-        for (String key : form.keySet()) {
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-            
-        }
-        //Почему так:
-        // 1) коллекции у объектов, полученных через JPA не являются обычными коллекциями,
-        // их нельзя просто заменить, можно только обновлять.
-        // 2) чтобы не городить каких-то сложных проверок, мы просто берем всё поля,
-        // полученные от пользователя и проверяем, являются ли они ролями,
-        // если да - устанавливаем их пользователю,
-        // обновляя таким образом нашу персистентную коллекцию ролей﻿
-
-
-        userRepo.save(user);
+        userSevice.saveUser(user,username,form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email",user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ){
+        userSevice.updateProfile(user,password,email);
+
+                return "redirect:/user/profile";
     }
 }
--- src/main/java/com/example/cloneTwitter/service/UserSevice.java
+++ src/main/java/com/example/cloneTwitter/service/UserSevice.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserSevice implements UserDetailsService {
@@ -39,6 +39,12 @@ public class UserSevice implements UserDetailsService {
 
         userRepo.save(user);
 
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
@@ -49,8 +55,6 @@ public class UserSevice implements UserDetailsService {
 
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -65,5 +69,64 @@ public class UserSevice implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String,String> form) {
+        user.setUsername(username);           // устанавливаем пользователю имя, которое пришло с фронта
+        //берем все существующие в приложении роли,
+        // преобразуем массив этих ролей в стрим (java stream api),
+        // где получаем имена ролей и полученый список имён складываем в set
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear(); //очищаем персистентную коллекцию от установленных ролей
+//обходим список полей, пришедших от пользователя,
+// проверяем, если имя какого-то поля является именем роли,
+// то ищем такую роль в enum Role и устанавливаем эти роли пользователю.
+        for (String key : form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+
+        }
+        //Почему так:
+        // 1) коллекции у объектов, полученных через JPA не являются обычными коллекциями,
+        // их нельзя просто заменить, можно только обновлять.
+        // 2) чтобы не городить каких-то сложных проверок, мы просто берем всё поля,
+        // полученные от пользователя и проверяем, являются ли они ролями,
+        // если да - устанавливаем их пользователю,
+        // обновляя таким образом нашу персистентную коллекцию ролей﻿
+
+
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+       boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+               (userEmail != null && userEmail.equals(email));
+
+       if(isEmailChanged){
+           user.setEmail(email);
+
+           if(!StringUtils.isEmpty(email)){
+               user.setActivationCode(UUID.randomUUID().toString());
+           }
+       }
+   if(!StringUtils.isEmpty(password)){
+           user.setPassword(password);
+   }
+
+   userRepo.save(user);
+
+       if(isEmailChanged) {
+           sendMessage(user);
+       }
     }
 }
