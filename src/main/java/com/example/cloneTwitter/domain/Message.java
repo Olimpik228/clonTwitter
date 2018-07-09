dfa5ab2ca84cd978d5d1f54641886ad4cd247dd9
Olimpik <olimpiewow@mail.ru>
2018-07-09 20:35:01 +0300

    Spring Boot: bean validation, шифрование паролей

--- src/main/java/com/example/cloneTwitter/domain/Message.java
+++ src/main/java/com/example/cloneTwitter/domain/Message.java
@@ -1,5 +1,8 @@
 package com.example.cloneTwitter.domain;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 
 @Entity //This tells Hibernate to make a table out of this class
@@ -8,7 +11,10 @@ public class Message {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message="Please fill the messgae")
+    @Length(max = 2048, message="Message too long (more than 2kB")
     private String text;
+    @Length(max = 255, message="Message too long (more than 255")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER) //one user have many messages, fetch = give with messages information about author
